08db1da2fcd6f85a571c6665ed29f4e681d92a3a
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-06-28 18:13:09 +0300

    Homework_11_1

--- src/main/java/ru/netology/javaqa38/Homework_10_1/RadioServise/Radio.java
+++ src/main/java/ru/netology/javaqa38/Homework_10_1/RadioServise/Radio.java
@@ -2,91 +2,85 @@ package ru.netology.javaqa38.Homework_10_1.RadioServise;
 
 public class Radio {
     // Параметры:
+
     private int numberRadioStations;
     private int volume;
+    private int maxNumberRadioStation;
 
-    // Сеттеры:
+    // Конструкторы:
 
-    public void setNumberRadioStations(int newNumberRadioStations) {
-        if (newNumberRadioStations < -1) {
-            return;
-        }
-        if (newNumberRadioStations > 10) {
-            return;
-        }
-        numberRadioStations = newNumberRadioStations;
+    public Radio() {
+        maxNumberRadioStation = 9;
     }
 
-    public void setVolume(int newVolume) {
-        if (newVolume < -1) {
-            return;
-        }
-        if (newVolume > 11) {
-            return;
-        }
-        volume = newVolume;
+    public Radio(int countRadioStation) {
+        maxNumberRadioStation = countRadioStation - 1;
+
     }
 
-
-    // Геттеры:
-    public int getNumberRadioStations() {
-        if (numberRadioStations == -1) {
-            return numberRadioStations = 9;
-        }
-        if (numberRadioStations == 10) {
-            return numberRadioStations = 0;
-        } else {
-            return numberRadioStations;
-        }
-    }
-
-    public int getVolume() {
-        if (volume == -1) {
-            return volume = 0;
-        }
-        if (volume == 11) {
-            return volume = 10;
-        } else {
-            return volume;
-        }
-    }
-
-    // Методы для проверки каналов радиостанции:
-
+    // Методы
     public void nextNumberRadioStation() {
-        if (numberRadioStations > -1) {
-            if (numberRadioStations < 10) {
-                int next = numberRadioStations + 1;
-                setNumberRadioStations(next);
-            }
+        if (numberRadioStations < maxNumberRadioStation) {
+            numberRadioStations++;
+        } else {
+            numberRadioStations = 0;
         }
     }
 
     public void prevNumberRadioStation() {
-        if (numberRadioStations > -1) {
-            if (numberRadioStations < 10) {
-                int prev = numberRadioStations - 1;
-                setNumberRadioStations(prev);
-            }
+        if (numberRadioStations > 0) {
+            numberRadioStations--;
+        } else {
+            numberRadioStations = maxNumberRadioStation;
         }
     }
 
-    //  Методы для проверки громкости:
     public void nextVolume() {
-        if (volume > 0) {
-            if (volume < 11) {
-                int next = volume + 1;
-                setVolume(next);
-            }
+        if (volume < 100) {
+            volume++;
+        } else {
+            volume = 100;
         }
+        ;
     }
 
     public void prevVolume() {
         if (volume > 0) {
-            if (volume < 11) {
-                int next = volume - 1;
-                setVolume(next);
-            }
+            volume--;
+        } else {
+            volume = 0;
         }
+    }
+
+    // Сеттеры:
+
+    public void setNumberRadioStations(int numberRadioStations) {
+        if (numberRadioStations < 0) {
+            return;
+        }
+        if (numberRadioStations > maxNumberRadioStation) {
+            return;
+        }
+        this.numberRadioStations = numberRadioStations;
+    }
+
+    public void setVolume(int volume) {
+        if (volume < 0) {
+            return;
+        }
+        if (volume > 100) {
+            volume = 100;
+        }
+        this.volume = volume;
+    }
+
+    // Геттеры:
+
+    public int getNumberRadioStations() {
+        return numberRadioStations;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 }
